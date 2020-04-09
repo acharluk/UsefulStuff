e0802947457692de4845d5ab287aa23bfe9d3fee
ACharLuk <acharluk@gmail.com>
2020-04-09 23:44:53 +0200

    Fix

---
+++ fct/README.md
@@ -0,0 +1,6 @@
+# Instalador WordPress
+
+Ejecuta el siguiente comando:
+```bash
+curl https://github.com/acharluk/UsefulStuff/blob/master/fct/instalaWP.sh | sudo bash
+```
--- fct/instalaWP.sh
+++ fct/instalaWP.sh
@@ -4,7 +4,6 @@ function __mysql {
     echo "Instalando MySQL"
     yum install mysql-server -y
     service mysqld start
-    
     echo "Configurando MySQL"
     echo 'CREATE USER usuario@localhost IDENTIFIED BY "usuario";
 CREATE DATABASE `wpdatabase`;
@@ -18,14 +17,12 @@ FLUSH PRIVILEGES;' > creaWPdb.sql
 function __apache2 {
     echo "Instalando Apache2"
     yum install httpd24 php72 php72-gd php72-pear php72-mysqlnd -y
-
     echo "Configurando Apache2"
     sed -i '151s/None/All/' /etc/httpd/conf/httpd.conf
     chown -R apache:apache /var/www
     chmod 2775 /var/www
     find /var/www -type d -exec sudo chmod 2775 {} \;
     find /var/www -type f -exec sudo chmod 0664 {} \;
-
     echo "Reiniciando Apache2"
     service httpd restart
 }
@@ -34,13 +31,11 @@ function __wordpress {
     echo "Descargando y configurando WordPress"
     wget https://wordpress.org/latest.tar.gz
     tar -xzf latest.tar.gz
-
     echo "Configurando WordPress"
     sed -e 's/database_name_here/wpdatabase/' \
         -e 's/username_here/usuario/'         \
         -e 's/password_here/usuario/'         \
         wordpress/wp-config-sample.php > wordpress/wp-config.php
-
     cp -r wordpress/* /var/www/html/
 }
 
