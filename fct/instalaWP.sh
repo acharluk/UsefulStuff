5137a78acc914d670a7f8969dcfed4d526c7f666
ACharLuk <acharluk@gmail.com>
2020-04-10 12:15:53 +0200

    Moar permisos

--- fct/instalaWP.sh
+++ fct/instalaWP.sh
@@ -36,6 +36,8 @@ function __wordpress {
     chmod 2775 /var/www
     find /var/www -type d -exec sudo chmod 2775 {} \;
     find /var/www -type f -exec sudo chmod 0664 {} \;
+    sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 100M/' /etc/php.ini
+
     echo "Reiniciando Apache2"
     service httpd restart
 }
